0262d8cd5ba02d90d609201d5573e4fb2cbe2eb6
Brian-Jaxon <brian.miller@jaxondigital.com>
2024-09-24 09:57:55 -0500

    IconDescriptor fpr Settings

--- Logic/Controllers/Navigation/HeaderBlockController.cs
+++ Logic/Controllers/Navigation/HeaderBlockController.cs
@@ -1,54 +1,52 @@
-﻿using JaxonFoundation.Logic.ViewModels;
-using JaxonFoundation.Logic.Models.Pages.Base;
-using Microsoft.AspNetCore.Mvc;
-using EPiServer.Web.Routing;
-using AutoMapper;
-using JaxonFoundation.Logic.Models.Pages;
-using JaxonFoundation.Logic.Interfaces;
-using JaxonFoundation.Logic.ViewModels.Navigation;
-using EPiServer.Web.Mvc;
-// using JaxonFoundation.Logic.Models.Pages.Error;
+﻿//using JaxonFoundation.Logic.ViewModels;
+//using JaxonFoundation.Logic.Models.Pages.Base;
+//using Microsoft.AspNetCore.Mvc;
+//using EPiServer.Web.Routing;
+//using AutoMapper;
+//using JaxonFoundation.Logic.ViewModels.Navigation;
+//using EPiServer.Web.Mvc;
 
 
-namespace JaxonFoundation.Logic.Controllers.Base
-{
-    public class HeaderBlockController : AsyncBlockComponent<HeaderBlock>
-    {
-        private readonly IContentLoader _contentLoader;
-        private readonly IPageRouteHelper _pageRouteHelper;
-        private readonly IMapper _mapper;
 
-        public HeaderBlockController(IContentLoader contentLoader, IPageRouteHelper pageRouteHelper, IMapper mapper)
-        {
-            this._contentLoader = contentLoader;
-            this._pageRouteHelper = pageRouteHelper;
-            this._mapper = mapper;
-        }
+//namespace JaxonFoundation.Logic.Controllers.Base
+//{
+//    public class HeaderBlockController : AsyncBlockComponent<HeaderBlock>
+//    {
+//        private readonly IContentLoader _contentLoader;
+//        private readonly IPageRouteHelper _pageRouteHelper;
+//        private readonly IMapper _mapper;
 
-        //[ChildActionOnly]
-        public IViewComponentResult Invoke()
-        {
-            ContentReference currentPageReference = this._pageRouteHelper.ContentLink;
+//        public HeaderBlockController(IContentLoader contentLoader, IPageRouteHelper pageRouteHelper, IMapper mapper)
+//        {
+//            this._contentLoader = contentLoader;
+//            this._pageRouteHelper = pageRouteHelper;
+//            this._mapper = mapper;
+//        }
 
-            if (ContentReference.IsNullOrEmpty(currentPageReference))
-                return Content(String.Empty);
+//        //[ChildActionOnly]
+//        public IViewComponentResult Invoke()
+//        {
+//            ContentReference currentPageReference = this._pageRouteHelper.ContentLink;
 
-            var currentPage = this._pageRouteHelper.Page;
-            BasePage homePage = GetHomePage((BasePage)currentPage);
-            var header = _mapper.Map<HomePage, HomePageViewModel>((HomePage)homePage);
-            return View("~/Views/Shared/A1Header.cshtml", header);
-        }
+//            if (ContentReference.IsNullOrEmpty(currentPageReference))
+//                return Content(String.Empty);
+
+//            var currentPage = this._pageRouteHelper.Page;
+//            BasePage homePage = GetHomePage((BasePage)currentPage);
+//            var header = _mapper.Map<HomePage, HomePageViewModel>((HomePage)homePage);
+//            return View("~/Views/Shared/A1Header.cshtml", header);
+//        }
 
 
-        private BasePage GetHomePage(BasePage currentPage)
-        {
-            ContentReference contentLink = currentPage.ContentLink;
-            var homePage = this._contentLoader.GetAncestors(contentLink)
-                .OfType<BasePage>()
-                .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("HomePage"))
-                .FirstOrDefault();
-            return homePage;
-        }
+//        private BasePage GetHomePage(BasePage currentPage)
+//        {
+//            ContentReference contentLink = currentPage.ContentLink;
+//            var homePage = this._contentLoader.GetAncestors(contentLink)
+//                .OfType<BasePage>()
+//                .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("HomePage"))
+//                .FirstOrDefault();
+//            return homePage;
+//        }
 
-    }
-}
+//    }
+//}
---
+++ Logic/Descriptors/PageTreeDescriptors/SettingsPageIconDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Shell;
+using JaxonFoundation.Logic.Constants;
+using JaxonFoundation.Logic.Interfaces.Descriptors;
+
+namespace JaxonFoundation.Logic.Descriptors.PageTreeDescriptors
+{
+    public class SettingsPageIconDescriptor : UIDescriptor<IHomePageIconDescriptor>
+    {
+        /// <summary>
+		/// Sets an icon for the home page.
+		/// A full list of icons can be found here: http://ux.episerver.com/#icons
+		/// </summary>
+		public SettingsPageIconDescriptor()
+        {
+            IconClass = EpiserverDefaultContentIcons.ActionIcons.Settings;
+        }
+    }
+}
