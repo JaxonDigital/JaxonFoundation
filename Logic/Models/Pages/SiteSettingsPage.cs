775a2ba223136faf9e0d67137ec61dc4213a6834
Brian-Jaxon <brian.miller@jaxondigital.com>
2024-10-02 11:40:43 -0500

    Set VisableInMenue to false for settings

--- Logic/Models/Pages/SiteSettingsPage.cs
+++ Logic/Models/Pages/SiteSettingsPage.cs
@@ -5,10 +5,11 @@ using JaxonFoundation.Logic.Constants;
 using JaxonFoundation.Logic.Interfaces.Descriptors;
 using System.ComponentModel.DataAnnotations;
 using JaxonFoundation.Infrastructure;
+using EPiServer.Cms.Shell.UI.ObjectEditing;
 
 namespace JaxonFoundation.Logic.Models.Pages
 {
-	[ContentType(DisplayName = "SiteSettings", GUID = "805D077A-B1FD-4BE3-8DC4-930B7FAC41BD",
+    [ContentType(DisplayName = "SiteSettings", GUID = "805D077A-B1FD-4BE3-8DC4-930B7FAC41BD",
 		Description = "Global Site Settings such Google Script and Robots.txt",
 		GroupName = PageGroups.UtilityPages)]
     [AvailableContentTypes(Availability.None, IncludeOn = new[] { typeof(HomePage) })]
@@ -17,7 +18,16 @@ namespace JaxonFoundation.Logic.Models.Pages
 
 	public class SiteSettingsPage : PageData, ISiteSettingsPageIcon , IAllPropertiesView
     {
-		[CultureSpecific]
+		public override void SetDefaultValues(EPiServer.DataAbstraction.ContentType contentType)
+		{
+			base.SetDefaultValues(contentType);
+			this.VisibleInMenu = false;
+        }
+
+		[ReloadOnChange]
+        public override bool VisibleInMenu { get ; set ; }
+
+        [CultureSpecific]
 		[UIHint(UIHint.Textarea)]
 		[Display(Name = "Google Analytics Identifier", Description = "Unique Id for GA Scripts", GroupName = SystemTabNames.Settings, Order = 100)]
 		public virtual string? GoogleAnanlyticsId { get; set; }
