dc17885033ccd0fd74f5aaa84d5f0e0340db4be5
Brian-Jaxon <brian.miller@jaxondigital.com>
2024-10-07 11:29:08 -0500

    Global Bools for Environment.IsProd() Environment.IsPrep() and Environment.IsDev()

---
+++ Infrastructure/Environmemts.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.ServiceLocation;
+
+public static class Environments
+{
+    private static readonly IWebHostEnvironment _webHostingEnvironment = ServiceLocator.Current.GetInstance<IWebHostEnvironment>();
+    public static bool IsProd()
+    {
+        var webhostingEnvironment = _webHostingEnvironment;
+        return webhostingEnvironment.IsProduction();
+    }
+
+    public static bool IsPrep()
+    {
+        var webhostingEnvironment = _webHostingEnvironment;
+        return webhostingEnvironment.IsStaging();
+    }
+
+    public static bool IsDev()
+    {
+        var webhostingEnvironment = _webHostingEnvironment;
+        return webhostingEnvironment.IsDevelopment();
+    }
+}
--- Infrastructure/SiteConfigurationPageFinder.cs
+++ Infrastructure/SiteConfigurationPageFinder.cs
@@ -1,36 +1,32 @@
 ﻿using EPiServer.ServiceLocation;
 using JaxonFoundation.Logic.Models.Pages;
-
-namespace JaxonFoundation.Infrastructure
+public static class SiteConfigurationPageFinder
 {
-    public class SiteConfigurationPageFinder
+    public static PageData? GetSiteConfigurationPage(PageData currentPage)
     {
-        public static PageData? GetSiteConfigurationPage(PageData currentPage)
+        var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+        if (currentPage is SiteConfigurationPage)
         {
-            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
-            if (currentPage is SiteConfigurationPage)
-            {
-                return currentPage;
-            }
+            return currentPage;
+        }
 
-            ContentReference contentLink = currentPage.ContentLink;
-            PageData? configurationPage;
-            if (currentPage.PageTypeName == "HomePage")
-            {
-                configurationPage = contentLoader.GetChildren<PageData>(contentLink)
-                .OfType<PageData>()
-                .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("ConfigurationPage"))
-                .FirstOrDefault();
-                return configurationPage;
-            }
-            else
-            {
-                configurationPage = contentLoader.GetAncestors(contentLink)
-                .OfType<PageData>()
-                .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("ConfigurationPage"))
-                .FirstOrDefault();
-                return configurationPage;
-            }
+        ContentReference contentLink = currentPage.ContentLink;
+        PageData? configurationPage;
+        if (currentPage.PageTypeName == "HomePage")
+        {
+            configurationPage = contentLoader.GetChildren<PageData>(contentLink)
+            .OfType<PageData>()
+            .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("ConfigurationPage"))
+            .FirstOrDefault();
+            return configurationPage;
+        }
+        else
+        {
+            configurationPage = contentLoader.GetAncestors(contentLink)
+            .OfType<PageData>()
+            .SkipWhile(x => x.ParentLink == null || !x.PageTypeName.EndsWith("ConfigurationPage"))
+            .FirstOrDefault();
+            return configurationPage;
         }
     }
 }
